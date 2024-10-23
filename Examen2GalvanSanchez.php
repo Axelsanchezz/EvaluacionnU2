56525f4ccefd87b0d07ccff8e9d1bc8380024b8a
Axelsanchezz <Galvanaxel492@gmail.com>
2024-10-23 09:49:20 -0600

    Add files via upload
    Aqui se almacena el Examen de la Unidad 2

---
+++ Examen2GalvanSanchez.php
@@ -0,0 +1,38 @@
+<?
+/* CBTIS 89
+Programa que guarda precios,descuentos y IVA.
+Axel Jesus Galvan Sanchez
+3°A Programación T.M.
+*/
+echo "ARREGLOS PRECIOS <br> ";
+$arrayPrecios = array(500,400,200,700,100);
+$arrayIVA=();
+$arraySubtotal=();
+$arrayDescuentos=()
+$arrayTotal=()
+echo "<br>";
+
+$longitud = count($arrayPrecios);
+
+echo " ARREGLOS PRECIOS <br> ";
+for($i=0; $i<$longitud; $i++)
+echo"<br>"}
+echo " ARREGLO IVA <br> ";
+for($i=0; $i<$longitud; $i++)
+	{echo $arrayPrecios["500","400","200","700","100"]*.16;
+echo"<br>"}
+echo " ARREGLO SUBTOTAL <br> ";
+for($i=0; $i<$longitud; $i++)
+	{
+echo"<br>"}
+echo " ARREGLO DESCUENTOS <br> ";
+for($i=0; $i<$longitud; $i++)
+	{
+echo"<br>"}
+echo " ARREGLO TOTAL <br> ";
+for($i=0; $i<$longitud; $i++)
+	{
+echo"<br>"}
+
+
+?>
